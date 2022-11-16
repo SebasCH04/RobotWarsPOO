af858a8b1e1483fdb60d44a49eb98639ec1e56c4
SebasCH04 <sebasch04@gmail.com>
2022-11-15 20:55:20 -0600

    Cambio #1

--- RobotWarsV3/src/main/java/model/common/IConstantsV2.java
+++ RobotWarsV3/src/main/java/model/common/IConstantsV2.java
@@ -5,15 +5,15 @@ import static model.common.IConstants.ARENA_HEIGTH;
 
 public interface IConstantsV2 {//Put new constants here!!!
     public static final int Y_AXIS_BORDER = ARENA_HEIGTH - 175;
-    public static final int LASER_1_SPEED = 4;
+    public static final int LASER_1_SPEED = 6;
     public static final int SLASH_SPEED = 1;
     public static final int STRIKE_GAP_X_Axis_R = 1;
     public static final int STRIKE_GAP_X_Axis_L = -160;
     public static final int STRIKE_GAP_Y_Axis = -50;
-    public static final int FIRE_TRAP_DAMAGE = 1;
-    public static final int SLASH_DAMAGE = 8;
-    public static final int SLASH_CONSUMPTION = 2;
+    public static final int FIRE_TRAP_DAMAGE = 10;
+    public static final int SLASH_DAMAGE = 10;
+    public static final int SLASH_CONSUMPTION = 5;
     public static final int BLASTER_DAMAGE = 5;
-    public static final int BLASTER_CONSUMPTION = 1;
+    public static final int BLASTER_CONSUMPTION = 2;
     public static final int ROBOT_FIGHTER_COOLDOWN = 20;
 }
